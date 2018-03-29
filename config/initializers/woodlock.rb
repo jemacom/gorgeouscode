43f8ae55b1d50bfac96535fd161e06360ed0b60b
Miguel Regedor <miguelregedor@gmail.com>
2018-03-29 15:05:26 +0200

    small fixes

--- config/initializers/woodlock.rb
+++ config/initializers/woodlock.rb
@@ -1,10 +1,10 @@
 Woodlock.setup do |config|
   config.site_name = "Gorgeous Code"
-  config.site_email = "noreply@regedor.com"
-  config.site_url = "https://gorgeouscode-alpha.herokuapp.com"
-  config.gravatar_default_url = "https://gorgeouscode-alpha.herokuapp.com/no_user.png"
+  config.site_email = "noreply@gorgeouscode.com"
+  config.site_url = "https://www.gorgeouscode.com"
+  config.gravatar_default_url = "https://www.gorgeouscode.com/no_user.png"
   config.authentication_services = ["github"]
   config.github_scope = "user:email, repo"
-  config.github_callback_url = "https://gorgeouscode-alpha.herokuapp.com/auth/github/callback"
+  config.github_callback_url = "https://www.gorgeouscode.com/auth/github/callback"
   # config.disable_welcome_email = true
 end
